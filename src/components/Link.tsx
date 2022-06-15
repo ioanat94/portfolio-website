23d72a075f60ca586caca97302fe693f14f9bee4
ioanat94 <ioanatiplea94@gmail.com>
2022-06-15 09:18:01 +0300

    Made Navbar disappear when link is clicked on mobile

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
 
 function Link({ name }) {
+  const toggleMenu = () => {
+    const btn = document.getElementById('menu-btn');
+    const nav = document.getElementById('menu');
+
+    btn.classList.toggle('open');
+    nav.classList.toggle('flex');
+    nav.classList.toggle('hidden');
+  };
+
   return (
     <a
       href={`#${name.toLowerCase()}`}
       className='text-mint md:transition-all md:hover:text-mintLight'
+      onClick={toggleMenu}
     >
       {name}
     </a>
