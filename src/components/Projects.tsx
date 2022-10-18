e4aa2322b448ca9e3283c68b594f70b1ec7c6579
Ioana Tiplea <ioanatiplea94@gmail.com>
2022-10-18 09:27:00 +0300

    Add new project

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -23,12 +23,12 @@ function Projects() {
         </div>
         <div className='flex flex-col gap-10 justify-evenly mt-2 md:mt-6 md:flex-row md:flex-wrap md:justify-between lg:flex-nowrap'>
           <Project
-            img={'/assets/projects/netflix.png'}
-            title={'Netflix Clone'}
-            gitLink={'https://github.com/ioanat94/netflix'}
-            liveLink={'https://netflix-clone-it.herokuapp.com/'}
+            img={'/assets/projects/petlify.png'}
+            title={'Petlify'}
+            gitLink={'https://github.com/ioanat94/petlify'}
+            liveLink={'https://petlify.vercel.app/'}
             desc={
-              'Netflix clone written using the MERN stack. Features a client side which allows users to register, login, browse and watch content categorized by type (movie or series) and genre. Also features an admin dashboard which is a fully functional CMS that allows admins to view, create, modify and delete users, content and content lists.'
+              'A pet shop application written using Typescript, React.js, Redux Toolkit, Node.js, MongoDB and Tailwind CSS. Users can sign in with Google, edit their profile, and order products. The app also features an admin dashboard which allows admins to view, create, modify and delete products, orders, users and admins.'
             }
           />
           <Project
@@ -41,12 +41,12 @@ function Projects() {
             }
           />
           <Project
-            img={'/assets/projects/portfolio.png'}
-            title={'Portfolio Website'}
-            gitLink={'https://github.com/ioanat94/portfolio-website'}
-            liveLink={'https://www.ioanatiplea.dev/'}
+            img={'/assets/projects/netflix.png'}
+            title={'Netflix Clone'}
+            gitLink={'https://github.com/ioanat94/netflix'}
+            liveLink={'https://netflix-clone-it.herokuapp.com/'}
             desc={
-              'My portfolio website, which you are looking at right now. The design was intended to be simple, clean and straightforward. Built with Tailwind CSS, Typescript and React.js/Next.js.'
+              'Netflix clone written using the MERN stack. Features a client side which allows users to register, login, browse and watch content categorized by type (movie or series) and genre. Also features an admin dashboard which is a fully functional CMS that allows admins to view, create, modify and delete users, content and content lists.'
             }
           />
         </div>
